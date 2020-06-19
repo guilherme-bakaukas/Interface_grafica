d8bdefda23a4900e26c29cff53e1c493fd8214b8
guilherme-bakaukas <61765419+guilherme-bakaukas@users.noreply.github.com>
2020-06-18 21:40:26 -0300

    organização na main setButton e setImage

--- InterfaceGrafica/src/App.java
+++ InterfaceGrafica/src/App.java
@@ -1,10 +1,31 @@
+import javax.swing.JButton;
 
 public class App {
 	
 	public static String DIRETORIO= App.class.getResource(".").getPath();
 	
 	public static void main(String[] args) {
-		new Janela(DIRETORIO+"lula.png", DIRETORIO+"dino.png");
+		Janela janela=new Janela();
+		
+		janela.setImage(DIRETORIO+"lula.png", DIRETORIO+"dino.png");
+		
+    	JButton right=new JButton("right");
+    	janela.setButton(right);
+    	
+    	JButton left=new JButton("left");
+    	janela.setButton(left);
+    	
+    	JButton up=new JButton("up");
+    	janela.setButton(up);
+    	
+    	JButton down=new JButton("down");
+    	janela.setButton(down);
+    	
+    	JButton random=new JButton("random");
+    	janela.setButton(random);
+		
+    	
+    	
 	}
 
 }
--- InterfaceGrafica/src/Janela.java
+++ InterfaceGrafica/src/Janela.java
@@ -8,12 +8,13 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class Janela extends JFrame{
 	private static final long serialVersionUID=7686762169698416749L;
 	private JPanel imagePane, controlPane;
 	
-	public Janela(String arquivo,String arquivo1){
+	public Janela(){
         super();
         setDefaultCloseOperation(EXIT_ON_CLOSE);//garante que a janela só fecha quando o usuário clica no exit
         setSize(300,300);
@@ -29,27 +30,13 @@ public class Janela extends JFrame{
         controlPane.setLayout(new FlowLayout());
         principalPane.add(controlPane,BorderLayout.SOUTH);
         
-        setImage(arquivo,arquivo1);
-        setButtom();
         setVisible(true);
 
     }
 
-    public void setButtom() {// inserir botao no painel de controle
-    	JButton right=new JButton("right");
-    	controlPane.add(right);
-    	
-    	JButton left=new JButton("left");
-    	controlPane.add(left);
-    	
-    	JButton up=new JButton("up");
-    	controlPane.add(up);
-    	
-    	JButton down=new JButton("down");
-    	controlPane.add(down);
-    	
-    	JButton random=new JButton("random");
-    	controlPane.add(random);
+    public void setButton(JButton botao) {// inserir botao no painel de controle
+    	controlPane.add(botao);
+    	SwingUtilities.updateComponentTreeUI(this);
 	}
 
 	public void setImage(String arquivo, String arquivo1) {//insere as imagens
@@ -65,6 +52,7 @@ public class Janela extends JFrame{
         	imagePane.add(space_image1);
         }
         
+        SwingUtilities.updateComponentTreeUI(this);
 
     }
 }
